b06fdf0f35037f2d1ffb33cecc55d68aaf5cc239
ChannabasavaH <channabasavahalemani49@gmail.com>
2024-11-29 22:27:09 +0530

    limited 10 num in the phone no field

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -32,11 +32,11 @@ const Register = () => {
       const res = await axios.post('/api/register', data);
       console.log(res);
       console.log("data saved successfully");
-      enqueueSnackbar("You have registered Successfully", {variant: "success"})
+      enqueueSnackbar("You have registered Successfully", { variant: "success" })
       navigate('/');
     } catch (error) {
       console.log(error);
-      enqueueSnackbar("Please Register Again", {variant: "error"})
+      enqueueSnackbar("Please Register Again", { variant: "error" })
     } finally {
       setLoading(false);
     }
@@ -53,19 +53,55 @@ const Register = () => {
         <form onSubmit={saveData} className='w-full border-2 border-sky-500 p-4 m-4 rounded-md md:w-1/2 lg:w-1/3'>
           <div>
             <label htmlFor="collegeName" className='text-xl font-lobster flex flex-col'>College Name</label>
-            <input type="text" placeholder='Enter College Name' id='collegeName' name='collegeName' value={collegeName} onChange={(event) => { setCollegeName(event.target.value) }} className='w-full p-2 m-2 border-black rounded-md' required/>
+            <input type="text" placeholder='Enter College Name' id='collegeName' name='collegeName' value={collegeName} onChange={(event) => { setCollegeName(event.target.value) }} className='w-full p-2 m-2 border-black rounded-md' required />
           </div>
           <div>
             <label htmlFor="category" className='text-xl font-lobster flex flex-col'>Group or Solo</label>
             <input type="text" placeholder='Group or Solo' id='category' name='category' value={category} onChange={(event) => { setCategory(event.target.value) }} className='w-full p-2 m-2 border-black rounded-md' required />
           </div>
           <div>
-            <label htmlFor="phoneNo" className='text-xl font-lobster flex flex-col'>Phone Number</label>
-            <input type="text" placeholder='Enter Phone Number' id='phoneNo' name='phoneNo' value={phoneNo} onChange={(event) => { setPhoneNo(event.target.value) }} className='w-full p-2 m-2 border-black rounded-md' required pattern="\d{10}" title="Phone number must be 10 digits"/>
+            <label htmlFor="phoneNo" className="text-xl font-lobster flex flex-col">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Phone Number"
+              id="phoneNo"
+              name="phoneNo"
+              value={phoneNo}
+              onChange={(event) => {
+                if (/^\d{0,10}$/.test(event.target.value)) {
+                  setPhoneNo(event.target.value);
+                }
+              }}
+              className="w-full p-2 m-2 border-black rounded-md"
+              required
+              maxLength="10"
+              pattern="\d{10}"
+              title="Phone number must be 10 digits"
+            />
           </div>
           <div>
-            <label htmlFor="altPhoneNo" className='text-xl font-lobster flex flex-col'>Alternate Phone Number</label>
-            <input type="text" placeholder='Enter Alternate Phone Number' id='altPhoneNo' name='altPhoneNo' value={altPhoneNo} onChange={(event) => { setAltPhoneNo(event.target.value) }} className='w-full p-2 m-2 border-black rounded-md' required pattern="\d{10}" title="Alternate phone number must be 10 digits"/>
+            <label htmlFor="altPhoneNo" className="text-xl font-lobster flex flex-col">
+              Alternate Phone Number
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Alternate Phone Number"
+              id="altPhoneNo"
+              name="altPhoneNo"
+              value={altPhoneNo}
+              onChange={(event) => {
+                if (/^\d{0,10}$/.test(event.target.value)) {
+                  setAltPhoneNo(event.target.value);
+                }
+              }}
+              className="w-full p-2 m-2 border-black rounded-md"
+              required
+              maxLength="10"
+              pattern="\d{10}"
+              title="Alternate phone number must be 10 digits"
+            />
           </div>
           <div className='flex flex-col justify-center items-center'>
             <h1 className='text-xl text-black mb-2'>Pay Through UPI ID or QR Code</h1>
@@ -75,7 +111,7 @@ const Register = () => {
           </div>
           <div>
             <label htmlFor="transaction" className='text-xl font-lobster flex flex-col'>Transaction Id</label>
-            <input type="text" placeholder='Please input your transaction id for registartion' id='transaction' name='transaction' value={transactionId} onChange={(event) => { setTransactionId(event.target.value) }} className='w-full p-2 m-2 border-black rounded-md' required/>
+            <input type="text" placeholder='Please input your transaction id for registartion' id='transaction' name='transaction' value={transactionId} onChange={(event) => { setTransactionId(event.target.value) }} className='w-full p-2 m-2 border-black rounded-md' required />
           </div>
           <div>
             <button className='bg-sky-500 text-black w-full border-black rounded p-2 m-2'>Register</button>
